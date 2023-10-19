1c2a01dfb357735ad9d0c9f55f831f12a62cbfee
Ricardo Garza <riccardogarza55@gmail.com>
2023-10-19 16:46:08 -0600

    Fix Postgres Connection

--- config/Database.js
+++ config/Database.js
@@ -2,18 +2,18 @@ import {Sequelize} from "sequelize";
 import dotenv from "dotenv";
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
-// const db = new Sequelize(process.env.POSTGRES_DB_URL, {
-//     dialect: "postgres",
-//     protocol: "postgres",
-//     dialectOptions: {
-//         ssl: JSON.parse(process.env.DB_SSL),
-//         native: true
-//     },
-// });
-
-const db = new Sequelize("auth_db","root","", {
-    dialect: "mysql",
-    host: "localhost"
+const db = new Sequelize(process.env.POSTGRES_DB_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+        ssl: JSON.parse(process.env.DB_SSL),
+        native: true
+    },
 });
+
+// const db = new Sequelize("auth_db","root","", {
+//     dialect: "mysql",
+//     host: "localhost"
+// });
 
 export default db;
